f104474521a65649cb0f61b9c5de0dc35d7e88c5
Vishnu Sivakoti NON DRI <vxs171101@darden.com>
2026-02-01 17:44:42 +0530

    Update API endpoint to use deployed Heroku backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8085';
+const API_BASE_URL = 'https://mantra-ias-bdb6c1377730.herokuapp.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
